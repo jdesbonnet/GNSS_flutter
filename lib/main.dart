1b9172da714d0ad5da56cd28de971b814b8477d1
Joe Desbonnet <jdesbonnet@gmail.com>
2024-11-27 22:01:01 +0000

    incorporate map + drawers

--- lib/main.dart
+++ lib/main.dart
@@ -10,35 +10,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
+void main() => runApp(const GNSSDemoApp());
 
+class GNSSDemoApp extends StatelessWidget {
+	const GNSSDemoApp({super.key});
 
-void main() {
-	LocationService.instance.init();
-	// Was getting textDirection related errors w/o MaterialApp wapper
-	//runApp(const ExampleApp());
-	runApp(  MaterialApp(home: GnssMapApp())  );
-}
+	static const appTitle = 'Flutter GNSS Demo';
 
-class GnssMapApp extends StatelessWidget {
-	const GnssMapApp({super.key});
-
-	/*
-	// Original location service demo
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
-			routes: {
-				Routes.main: (_) => const MainPage(),
-			},
-			initialRoute: Routes.main,
+			title: appTitle,
+			//home: MyHomePage(title: appTitle),
+			home: buildAppWidget(context)
 		);
 	}
-	*/  
   
-	// from https://docs.fleaflet.dev/
+  
+  
+
 	@override
-	Widget build(BuildContext context) {
-		Widget w = buildMapWidget(context);
+	Widget buildAppWidget(BuildContext context) {
+
 		Widget sw = Scaffold (
 			appBar: AppBar(title: const Text ('Flutter GNSS Demo')),
 			drawer: Drawer (
@@ -50,40 +43,17 @@ class GnssMapApp extends StatelessWidget {
 						ListTile(title: const Text('Item 1'), onTap: () {},),
 						ListTile(title: const Text('Item 2'), onTap: () {},),
 						ListTile(title: const Text('Item 3'), onTap: () {},),
-						//w
 					],
 				),
 			),
+			body: buildMapWidget(context)
 		);
 
 		return sw;
-		/*
-		return FlutterMap(
-				options: MapOptions(
-				initialCenter: LatLng(53.28, -8.98),
-				initialZoom: 14,
-			),
-			children: [
-				TileLayer( 
-					urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-					userAgentPackageName: 'com.test.exampleapp',
-				),
-          
-				MarkerLayer( markers: [
-					Marker(point: LatLng(53.2856, -8.9827),width: 32, height: 32, child: FlutterLogo(),),
-				]),
 
-
-				RichAttributionWidget( 
-					attributions: [
-						TextSourceAttribution('OpenStreetMap contributors'),
-					],
-				),      
-			],
-		); // end FlutterMap constructor
-		*/
 	}
 	
+	// from https://docs.fleaflet.dev/
 	Widget buildMapWidget(BuildContext context) {
 			return FlutterMap(
 				options: MapOptions(
@@ -109,5 +79,8 @@ class GnssMapApp extends StatelessWidget {
 			],
 		); // end FlutterMap constructor
 	}
+	
+	
+	
 }
 
